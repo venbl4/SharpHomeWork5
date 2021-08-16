10b5eaf5e974f284e0c15511707c9dd1710273d2
venbl4 <lebmax2@gmail.com>
2021-08-16 16:23:48 +0700

    На проверку

---
+++ CoorectLogin/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace CoorectLogin
+{
+    class Program
+    {
+
+        //Метод проверки на соответствие логина требованиям
+        static bool CheckLogin(string login)
+        {
+            int length = login.Length;
+            if (length >= 2 && length <= 10)
+            {
+                bool check = true;
+                char letter = login[0];
+                if (Char.IsDigit(letter))
+                    return false;
+                for (int i = 1; i < length; i++)
+                {
+                    letter = login[i];
+                    if (!(Char.IsDigit(letter) || IsLatinLetter(letter)))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    return true;
+            }
+            return false;
+        }
+
+
+        //Метод проверяет, что символ - латинская буква
+        private static bool IsLatinLetter(char letter)
+        {
+            int num = letter;
+            if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
+                return true;
+            else
+                return false;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа проверки корректности логина.");
+            int AmountOfTries = 3;
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+
+                if (CheckLogin(login))
+                {
+                    Console.WriteLine("Логин корректен!");
+                    break;
+                }
+                else
+                {
+                    AmountOfTries--;
+                    Console.WriteLine("Неверный ввод логина. \nДолжны быть соблюдены следующие условия:"
+                        + "\nдлина строки 2 до 10 символов;"
+                        + "\nбуквы только латинского алфавита или цифры;"
+                        + "\nцифра не может быть первой."
+                        + Environment.NewLine + "У Вас осталось " + AmountOfTries + "попыток");
+                }
+
+               
+            } while (AmountOfTries > 0); 
+
+                if(AmountOfTries == 0)
+            {
+                Console.WriteLine("У вас не осталось попыток! Приходите в следующий раз.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
